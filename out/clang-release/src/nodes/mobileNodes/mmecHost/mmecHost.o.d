70a1dd4cd471ae58426e5238846b0bfe569542f8
Wynn-TH <zhth311@163.com>
2025-03-13 13:59:31 +0000

    network building finished. error in the mobile module in antenna

---
+++ out/clang-release/src/nodes/mobileNodes/mmecHost/mmecHost.o.d
@@ -0,0 +1,8 @@
+../out/clang-release/src/nodes/mobileNodes/mmecHost/mmecHost.o: \
+  nodes/mobileNodes/mmecHost/mmecHost.cc \
+  nodes/mobileNodes/mmecHost/mmecHost.h \
+  /home/tianhao/Documents/omnetpp-6.1/include/omnetpp.h
+
+nodes/mobileNodes/mmecHost/mmecHost.h:
+
+/home/tianhao/Documents/omnetpp-6.1/include/omnetpp.h:
---
+++ out/clang-release/src/nodes/mobileNodes/ue/ue.o.d
@@ -0,0 +1,7 @@
+../out/clang-release/src/nodes/mobileNodes/ue/ue.o: \
+  nodes/mobileNodes/ue/ue.cc nodes/mobileNodes/ue/ue.h \
+  /home/tianhao/Documents/omnetpp-6.1/include/omnetpp.h
+
+nodes/mobileNodes/ue/ue.h:
+
+/home/tianhao/Documents/omnetpp-6.1/include/omnetpp.h:
