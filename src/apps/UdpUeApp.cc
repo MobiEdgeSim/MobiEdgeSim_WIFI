6296bb9ebc39341cb115fba5b4ced550e1d0aab7
Tianhao Zhang <zhth311@163.com>
2025-04-07 22:15:41 +0100

    for loop added and formatted

--- src/apps/UdpUeApp.cc
+++ src/apps/UdpUeApp.cc
@@ -25,18 +25,16 @@
 namespace MobiEdgeSim {
 
 Define_Module(UdpUeApp);
-void UdpUeApp::initialize(int stage)
-{
+void UdpUeApp::initialize(int stage) {
     inet::UdpBasicApp::initialize(stage);
     if (stage == inet::INITSTAGE_LOCAL) {
         // 创建更新目的地址的自消息，间隔从参数读取
         updateDestInterval = par("updateDestInterval").doubleValue();
         updateDestMsg = new cMessage("updateDestAddresses");
-        scheduleAt(simTime() +1+ updateDestInterval, updateDestMsg);//add 1s as start time
+        scheduleAt(simTime() + 1 + updateDestInterval, updateDestMsg); //add 1s as start time
     }
 }
-void UdpUeApp::handleMessage(cMessage *msg)
-{
+void UdpUeApp::handleMessage(cMessage *msg) {
     if (msg->isSelfMessage()) {
         if (strcmp(msg->getName(), "updateDestAddresses") == 0) {
             updateDestAddresses();
@@ -48,30 +46,35 @@ void UdpUeApp::handleMessage(cMessage *msg)
     // 非自消息则调用父类处理
     inet::UdpBasicApp::handleMessage(msg);
 }
-void UdpUeApp::updateDestAddresses()
-{
+void UdpUeApp::updateDestAddresses() {
     EV << "UdpUeApp::updateDestAddresses invoked" << "\n";
     // 获取 orchestrator 模块
     // 假设 orchestrator 的 NED 模块名称为 "orchestrator" 位于网络层的某处，例如 "network.orchestrator"
     // 你可以根据实际情况修改路径
-    MobiEdgeSim::Orchestrator *orch = check_and_cast<MobiEdgeSim::Orchestrator*>(getSimulation()->getSystemModule()->getSubmodule("orchestrator"));
+    MobiEdgeSim::Orchestrator *orch =
+            check_and_cast<MobiEdgeSim::Orchestrator*>(
+                    getSimulation()->getSystemModule()->getSubmodule(
+                            "orchestrator"));
 
     if (!orch) {
-        EV_WARN << "Orchestrator module not found, cannot update destination addresses.\n";
+        EV_WARN
+                       << "Orchestrator module not found, cannot update destination addresses.\n";
         return;
     }
     // 这里构造一个当前位置，可以从自身的 mobility 模块获取
     inet::Coord currentCoord;
     cModule *mobilityModule = getParentModule()->getSubmodule("mobility");
     if (mobilityModule) {
-        auto mobility = check_and_cast<veins::VeinsInetMobility*>(mobilityModule);
+        auto mobility = check_and_cast<veins::VeinsInetMobility*>(
+                mobilityModule);
         currentCoord = mobility->getCurrentPosition();
     } else {
         currentCoord.x = par("latitude").doubleValue();
         currentCoord.y = par("longitude").doubleValue();
         currentCoord.z = 0;
     }
-    EV<<"current latitude"<<currentCoord.x<<"longitude"<<currentCoord.y<<endl;
+    EV << "current latitude" << currentCoord.x << "longitude" << currentCoord.y
+              << endl;
     // 调用 orchestrator 的接口获取范围内的 MEC host 名称
     std::vector<std::string> hostNames = orch->getMechostNames(currentCoord);
 
@@ -104,49 +107,62 @@ void UdpUeApp::updateDestAddresses()
     }
 
 }
-void UdpUeApp::processPacket(inet::Packet *packet)
-{
-    EV << "UdpUeApp::processPacket received packet: " << packet->getName() << endl;
-    const auto& appPkt = packet->peekAtFront<TimestampedPacket>();
+void UdpUeApp::processPacket(inet::Packet *packet) {
+    EV << "UdpUeApp::processPacket received packet: " << packet->getName()
+              << endl;
+    const auto &appPkt = packet->peekAtFront<TimestampedPacket>();
 
     simtime_t delay = simTime() - appPkt->getSendTime();
     EV << "RTT delay = " << delay << "s" << endl;
 
     auto srcAddrTag = packet->findTag<inet::L3AddressInd>();
     if (srcAddrTag) {
-            auto addr = srcAddrTag->getSrcAddress();
-            auto *host = inet::L3AddressResolver().findHostWithAddress(addr);
-            std::string hostName = host ? host->getFullName() : addr.str();
-            rttMap[hostName] = delay;
-            EV << "Saved RTT to rttMap[" << hostName << "] = " << delay << "s" << endl;
-        }
+        auto addr = srcAddrTag->getSrcAddress();
+        auto *host = inet::L3AddressResolver().findHostWithAddress(addr);
+        std::string hostName = host ? host->getFullName() : addr.str();
+        rttMap[hostName] = delay;
+        EV << "Saved RTT to rttMap[" << hostName << "] = " << delay << "s"
+                  << endl;
+    }
     // 父类处理
     UdpBasicApp::processPacket(packet);
 }
 
-void UdpUeApp::finish()
-{
+void UdpUeApp::finish() {
     cancelAndDelete(updateDestMsg);
     inet::UdpBasicApp::finish();
 }
 
-void UdpUeApp::sendPacket()
-{
-    auto packet = new inet::Packet("UdpBasicAppData");
+void UdpUeApp::sendPacket() {
+    for (auto &addr : destAddresses) {
+        // 每个地址都要做一次新建packet，以免多次sendTo同一个对象
+        auto packet = new inet::Packet("UdpBasicAppData");
+        const auto &appPkt = inet::makeShared<TimestampedPacket>();
+        appPkt->setSendTime(simTime());
+        appPkt->setSequenceNumber(numSent);
+        appPkt->setPayloadSize(par("messageLength").intValue());
+        appPkt->setChunkLength(inet::B(par("messageLength")));
 
-    const auto& appPkt = inet::makeShared<TimestampedPacket>();
-    appPkt->setSendTime(simTime());
-    appPkt->setSequenceNumber(numSent);
-    appPkt->setPayloadSize(par("messageLength").intValue());
-    appPkt->setChunkLength(inet::B(par("messageLength")));
+        packet->insertAtBack(appPkt);
+        emit(inet::packetSentSignal, packet);
+        socket.sendTo(packet, addr, destPort);
+        numSent++;
+    }
 
-    packet->insertAtBack(appPkt);
-    emit(inet::packetSentSignal, packet);
-    socket.sendTo(packet, chooseDestAddr(), destPort);
-    numSent++;
+//    auto packet = new inet::Packet("UdpBasicAppData");
+//
+//    const auto& appPkt = inet::makeShared<TimestampedPacket>();
+//    appPkt->setSendTime(simTime());
+//    appPkt->setSequenceNumber(numSent);
+//    appPkt->setPayloadSize(par("messageLength").intValue());
+//    appPkt->setChunkLength(inet::B(par("messageLength")));
+//
+//    packet->insertAtBack(appPkt);
+//    emit(inet::packetSentSignal, packet);
+//    socket.sendTo(packet, chooseDestAddr(), destPort);
+//    numSent++;
 
 }
-
 
 //std::ostringstream str;
 //str << packetName << "-" << numSent;
@@ -163,5 +179,5 @@ void UdpUeApp::sendPacket()
 //socket.sendTo(packet, destAddr, destPort);
 //numSent++;
 
-} // namespace MobiEdgeSim
+}// namespace MobiEdgeSim
 
